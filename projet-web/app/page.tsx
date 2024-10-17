5dfa5725cac4d13d426d607a59018cf32e2333b7
Ibrahim Ayachi <ibrahim.ayachi@student.vinci.be>
2024-10-17 19:52:15 +0200

    début de navBar + test

--- projet-web/app/page.tsx
+++ projet-web/app/page.tsx
@@ -1,17 +1,25 @@
-import Link from "next/link";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold text-center text-red-600">
-                Bienvenue sur le site qui a pour but de vous présenter NextJs
-            </h1>
-            <div>
-                <p className="text-xl font-semi-bold">
-                    En tout premier voici un lien vers la page de connexion
-                </p>
-                <Link href="/authentification" className="text-xl font-bold text-blue-600"> Connexion</Link>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+        <Navbar />
+    <div className="p-4 m-4">
+      <h1 className="text-3xl font-extrabold text-center text-blue-600 mb-6">
+        Welcome to Next.js!
+      </h1>
+      <h2 className="text-lg text-gray-700 leading-relaxed text-left mb-4">
+        Nous avons créé ce site pour vous répertorier les différentes <br />
+        fonctionnalités de Next.js qui vont vous faciliter la vie.
+      </h2>
+      <ul className="list-disc list-inside space-y-2 text-gray-800">
+        <li>Routage simplifié</li>
+        <li>Rendu côté serveur (SSR) et génération statique (SSG)</li>
+        <li>Optimisation automatique des images</li>
+        <li>API intégrées</li>
+        <li>Prise en charge des CSS modules et Tailwind CSS</li>
+      </ul>
+    </div>
+    </div>
+  );
 }
