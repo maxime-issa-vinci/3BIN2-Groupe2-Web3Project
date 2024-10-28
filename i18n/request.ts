6dd78ce4f92857a0ccbc9da824cd39cca2744612
Ibrahim <ibrahim.ayachi@student.vinci.be>
2024-10-28 15:00:46 +0100

    add internalisation between page

--- i18n/request.ts
+++ i18n/request.ts
@@ -4,12 +4,13 @@ import {routing} from './routing';
 export default getRequestConfig(async ({requestLocale}) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
+
  
   // Ensure that a valid locale is used
   if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale;
   }
- 
+  
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default
